c89f09a291d8651df3033ea6eb2fad15a0bb9462
Francisco Reyes <130443772+reyesfr24@users.noreply.github.com>
2024-11-11 14:08:34 +0100

    Añadido useEffect y estilos de la bola

--- projects/03-mouse-followerd/src/App.jsx
+++ projects/03-mouse-followerd/src/App.jsx
@@ -1,11 +1,40 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
+  const [enabled, setEnabled] = useState(false)
+
   useEffect(()=> {
-    console.log('efecto')
-  })
-  return (
-    <h3>Proyecto 3</h3>
+    console.log('effect', { enabled })
+  
+    const handleMove = (event) => {
+      const { clientX, clientY} = event
+      console.log('handleMove', { clientX, clientY })
+    }
+  
+    window.addEventListener('pointermove', handleMove)
+  
+  }, [enabled])
+
+  return(
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity:0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: 'translate(0px, 0px)'
+      }}
+      />
+      
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
   )
 }
 
