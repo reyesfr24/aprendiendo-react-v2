a8801fe29bcb34f3cc33e91b5e6e74bd637af784
Francisco Reyes <130443772+reyesfr24@users.noreply.github.com>
2024-11-27 08:43:32 +0100

    Explicacion de hooks

--- projects/03-mouse-followerd/src/App.jsx
+++ projects/03-mouse-followerd/src/App.jsx
@@ -1,8 +1,16 @@
 import { useEffect, useState } from "react"
 
 function App() {
+  
+  // Hook useState: al cambiar vuelve a renderizar el componente para reflejar los cambios en la interfaz
   const [enabled, setEnabled] = useState(false)
 
+  /* Hook useEffect: permite ejectutar código al montarse el componente en el DOM (minimo esta vez se ejecuta) y cada vez 
+  que cambian las dependencias que le indiquemos. El código a ejecutar va entre llaves {} y al final las dependencias 
+  entre [] (un array). 
+  ATENCIÓN: Si no se le pasa NADA como dependencia, se ejecutara el código cada vez que se renderice
+  el componente. Si se le pasa un array vacío: [], solo se ejecutara la primera vez que renderice y si se le especifica */
+  
   useEffect(()=> {
     console.log('effect', { enabled })
   
