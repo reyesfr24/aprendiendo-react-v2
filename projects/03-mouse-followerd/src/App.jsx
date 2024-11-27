2fee5fc077a79c2f30219e4ede09553acabc7599
Francisco Reyes <130443772+reyesfr24@users.noreply.github.com>
2024-11-27 11:34:16 +0100

    Acabado

--- projects/03-mouse-followerd/src/App.jsx
+++ projects/03-mouse-followerd/src/App.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react"
 
-function App() {
+const FollowMouse = () => {
   
-  // Hook useState: al cambiar vuelve a renderizar el componente para reflejar los cambios en la interfaz
+  // Hook useState: al cambiar el estado vuelve a renderizar el componente para reflejar los cambios en la interfaz
   const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0})
 
   /* Hook useEffect: permite ejectutar código al montarse el componente en el DOM (minimo esta vez se ejecuta) y cada vez 
   que cambian las dependencias que le indiquemos. El código a ejecutar va entre llaves {} y al final las dependencias 
@@ -17,14 +18,24 @@ function App() {
     const handleMove = (event) => {
       const { clientX, clientY} = event
       console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY})
     }
-  
-    window.addEventListener('pointermove', handleMove)
-  
+
+    // Suscripción a un evento de manejo del ratón
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    /* Para limpiar el los eventos (así eliminamos las suscripciones que seguirán activas) utlizamos return.
+      Esto limpiara cada vez que se desmonte el componente o que cambien las dependencias */
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+
   }, [enabled])
 
-  return(
-    <main>
+  return (
+    <>
       <div style={{
         position: 'absolute',
         backgroundColor: '#09f',
@@ -35,13 +46,22 @@ function App() {
         top: -20,
         width: 40,
         height: 40,
-        transform: 'translate(0px, 0px)'
+        transform: `translate(${position.x}px, ${position.y}px)`
       }}
       />
       
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? 'Desactivar' : 'Activar'} seguir puntero
       </button>
+    </>
+  )
+}
+
+// El cuerpo de las funciones de componentes se ejecutan cada vez que se renderiza el componente
+function App() {
+  return(
+    <main>
+      <FollowMouse />
     </main>
   )
 }
